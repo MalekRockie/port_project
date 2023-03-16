8b7338b6067f3368112de629af64721d69a1402f
AbdulMalek <M_2008_HA@YAHOO.COM>
2023-03-16 03:54:43 -0400

    Adjusted the styling of the nav bar elements, made the text white and the hover color grey

--- src/components/Headerpage.jsx
+++ src/components/Headerpage.jsx
@@ -3,27 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
-const navbarStyle = {
-  height: '300px'
-};
-
-const navLinkStyle = {
-  fontSize: '25px',
-  margin: '0 40px'
-};
+import './css/HeaderPage.css';
 
 export default function HeaderPage() {
   return (
-    <Navbar bg="light" expand="lg" style={navbarStyle} className="navbar-expand justify-content-center">
+    <Navbar bg="dark" expand="lg" className="navbar-container navbar-custom">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            <Nav.Link href="#skills" style={navLinkStyle}>Skills</Nav.Link>
-            <Nav.Link href="#projects" style={navLinkStyle}>Projects</Nav.Link>
-            <Nav.Link href="#education" style={navLinkStyle}>Education</Nav.Link>
-            <Nav.Link href="#contacts" style={navLinkStyle}>Contact me</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#education">Education</Nav.Link>
+            <Nav.Link href="#contacts">Contact me</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
