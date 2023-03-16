1f0cb83112074577a41c5b315cacbab84d9a26ef
AbdulMalek <M_2008_HA@YAHOO.COM>
2023-03-16 15:04:40 -0400

    Adjusted nav bar items, still need to all page label name under nav items in center

--- src/components/Headerpage.jsx
+++ src/components/Headerpage.jsx
@@ -7,17 +7,14 @@ import './css/HeaderPage.css';
 
 export default function HeaderPage() {
   return (
-    <Navbar bg="dark" expand="lg" className="navbar-container navbar-custom">
+    <Navbar bg="dark" expand="lg" className="navbar-container navbar-expand navbar-custom">
       <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#education">Education</Nav.Link>
-            <Nav.Link href="#contacts">Contact me</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+            <Nav className="mx-auto pb-6" >
+              <Nav.Link href="#skills">Skills</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#education">Education</Nav.Link>
+              <Nav.Link href="#contacts">Contact me</Nav.Link>
+            </Nav>
       </Container>
     </Navbar>
   );
