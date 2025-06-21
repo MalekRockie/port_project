a3afe04297dfe189dbb2bd5d177e0ca744dc3623
Abdulmalek Abulgasem <m_2008_ha@yahoo.com>
2025-06-21 19:28:43 -0400

    Adjusted the navigation bar by making it fixed on screen, Added a Home button to smoothly scroll to the top of the page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-scroll';
+import { Link, animateScroll as scroll } from 'react-scroll';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -13,15 +13,23 @@ import IconButton from '@mui/material/IconButton';
 
 export default function HeaderPage() {
 
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+  const handleSetActive = (to) => {
+    console.log(to);
+  };
+
   return (
     <div>
       {/* <div id="particles-js" style={{ position: 'absolute', width: '100%', height: '100%' }}></div> */}
       <div className="navBarContainer">
         <div className='navTitle'>ABDULMALEK ABULGASEM</div>
           <div className="container purple topBotomBordersIn">
+            <Link onClick={scrollToTop} smooth={true} duration={1000}>Home</Link>
             <Link to="projects" smooth={true} duration={1000}>Projects</Link>
             <Link to="skillSection" smooth={true} duration={1000}>Skills</Link>
-            <Link to="education" smooth={true} duration={1000}>Education</Link>
+            {/* <Link to="education" smooth={true} duration={1000}>Education</Link> */}
             <Link to="contact" smooth={true} duration={1000}>Contact me</Link>
           </div>
       </div>
